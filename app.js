7b870ebb69657bac1290f2fabf5ffd821323ee00
Shivam Raj <shivam8800@Viksits-MacBook-Pro.local>
2018-06-20 00:19:12 +0530

    complete profit feature

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ $(document).ready( function () {
 		['bytecoin','BCN','http://bcn.superpools.online:8117/stats'], 
 		['digitalnote','XDN', 'http://xdn.superpools.online:8117/stats'], 
 		[ 'monero_original','XMO', 'http://xmo.superpools.online:8117/stats'],
-		[ 'monero_classic','XMO', 'http://xmo.superpools.online:2117/stats'], 
+		[ 'monero_classic','XMC', 'http://xmo.superpools.online:2117/stats'], 
 		['dero','DERO', 'http://dero.superpools.online:1117/stats'], 
 		['sumo','SUMO', 'http://sumo.superpools.online:4117/stats'], 
 		['karbo','KRB', 'http://krb.superpools.online:8117/stats'], 
@@ -28,46 +28,49 @@ $(document).ready( function () {
     });
 
 	window.setInterval(function(){
-		profitCalculation(list_coins);
+		getDetails(list_coins);
 	}, 300000);
 
-	function finalProfit(){
+	function finalProfit(coin_difficulty,coin_reward, coin){
 		var hashRate = $('#hashrate').val();
-		var profit = (hashRate * 86400 / coin_difficulty) * coin_reward
-		return profit
+		if (hashRate == ""){
+			profit = "NAN"
+		} else {
+			var profit = (hashRate * 86400 / coin_difficulty) * coin_reward
+		}
+		var profit_of_coin = {}
+		profit_of_coin[coin] = profit
+		return profit_of_coin
 	}
 
 	coin_details = {}
 	getDetails(list_coins);
 
 	function getDetails(main_list){
+		var count =0;
 		main_list.forEach(function(item){
 		    $.ajax({
 				url: item[2],
 				type: 'GET',
 				success: function(success) {
-					// console.log(success.network.difficulty , "name", item[1], "reward", success.lastblock);
 					if (success.lastblock == undefined){
-						index =0;
-						while (index < 9){
-							$.ajax({
-								url: item[2],
-								type: 'GET',
-								success: function(json) {
-									console.log(index);
-									console.log(json.lastblock);
-									index++;
-								}
-							});
+						if (success.network.reward){
+							coin_details[item[1] + "-difficulty"] = success.network.difficulty;
+							coin_details[item[1] + "-reward"] = success.network.reward;
 						}
 					} else {
 						coin_details[item[1] + "-difficulty"] = success.network.difficulty;
 						coin_details[item[1] + "-reward"] = success.lastblock.reward;
-						console.log(success.lastblock.reward)
+					}
+					count++;
+					if (count == 9){
+						for (var i =0; i < main_list.length; i++){
+							// console.log(main_list[i][1], coin_details[main_list[i][1].toString() +"-difficulty"])
+							finalProfit(coin_details[main_list[i][1].toString() +"-difficulty"], coin_details[main_list[i][1].toString() +"-reward"], main_list[i][1]);
+						}
 					}
 				}
 			});
-			
 		});
 	}
 
