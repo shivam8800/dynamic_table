21e9e366a8ce512ab553b889b21eddcb360bd440
apoorvelous <apoorv17@navgurukul.org>
2018-06-21 12:16:47 +0530

    Added the Links to get prices and Fixed getDetail function

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ $(document).ready( function () {
 	  $('[data-toggle="popover"]').popover()
 	});
 
+	//Varaible containing the List of all the Coins and Important Details
 	var list_coins = [ 
 		['bytecoin','BCN','http://bcn.superpools.online:8117/stats'], 
 		['digitalnote','XDN', 'http://xdn.superpools.online:8117/stats'], 
@@ -27,10 +28,12 @@ $(document).ready( function () {
         console.log("fds",coin_details);
     });
 
+	//To get the difficulty and rewards of each pool after every 5 minutes
 	window.setInterval(function(){
 		getDetails(list_coins);
 	}, 300000);
 
+	//Function Defined to get the Final Profit
 	function finalProfit(coin_difficulty,coin_reward, coin){
 		var hashRate = $('#hashrate').val();
 		if (hashRate == ""){
@@ -43,9 +46,11 @@ $(document).ready( function () {
 		return profit_of_coin
 	}
 
+	//Calling the Function to get Details and Creating a disctionary of collected data.
 	coin_details = {}
 	getDetails(list_coins);
 
+	//Main Function Defined to get the details of the Coin (Rewards and Difficulty)
 	function getDetails(main_list){
 		var count =0;
 		main_list.forEach(function(item){
@@ -54,10 +59,16 @@ $(document).ready( function () {
 				type: 'GET',
 				success: function(success) {
 					if (success.lastblock == undefined){
-						if (success.network.reward){
-							coin_details[item[1] + "-difficulty"] = success.network.difficulty;
-							coin_details[item[1] + "-reward"] = success.network.reward;
+						// if (success.network.reward){
+						// 	coin_details[item[1] + "-difficulty"] = success.network.difficulty;
+						// 	coin_details[item[1] + "-reward"] = success.network.reward;
+						// }
+						var currentTime = new Date().getTime();
+						while (currentTime + 2000 >= new Date().getTime()){
+
 						}
+						coin_details[item[1] + "-difficulty"] = success.network.difficulty;
+						coin_details[item[1] + "-reward"] = success.lastblock.reward;
 					} else {
 						coin_details[item[1] + "-difficulty"] = success.network.difficulty;
 						coin_details[item[1] + "-reward"] = success.lastblock.reward;
@@ -72,6 +83,20 @@ $(document).ready( function () {
 				}
 			});
 		});
-	}
+	};
+
+	//Variable defined to store the links of the Coins who's prices are available on the Crypto Compare API
+	//Look for current_price
+	var coin_price = [
+	['bytecoin', 'BCN', 'https://api.coingecko.com/api/v3/coins/bytecoin'],
+	['digitalnote','XDN', 'https://api.coingecko.com/api/v3/coins/digitalnote'], 
+	['monero_original','XMO', 'https://api.coingecko.com/api/v3/coins/monero-original'],
+	['monero_classic','XMC', 'https://api.coingecko.com/api/v3/coins/monero-classic'],
+	['dero','DERO', 'https://api.coingecko.com/api/v3/coins/dero'],
+	['sumo','SUMO', 'https://api.coingecko.com/api/v3/coins/sumokoin'], 
+	['karbo','KRB', 'https://api.coingecko.com/api/v3/coins/karbo'],
+	['newton','NCP', 'https://api.coingecko.com/api/v3/coins/newton-coin-project'], 
+	['plura','PLURA', 'https://api.coingecko.com/api/v3/coins/pluracoin']
+	]
 
 } );
